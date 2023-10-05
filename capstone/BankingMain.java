0d4c3bd4f3d2bb6c85629d96ee449e43f240c44d
Wilsonlky7 <102144875+Wilsonlky7@users.noreply.github.com>
2023-10-05 10:45:52 +0800

    Update BankingMain.java

--- capstone/BankingMain.java
+++ capstone/BankingMain.java
@@ -1,29 +1,45 @@
 package capstone;
 
-import java.util.Scanner;
+import java.util.Scanner
 
-public class BankingMain {
-    public static void main(String[] args) {
-        Customer cObj;
+public class BankingMain{
+
+    public static void main[String[] args]{
         Scanner sc = new Scanner(System.in);
         int choice = 0;
-        while(true){
+        while (true){
+            
+            System.out.println ( x:"1 -> Register Customer");
+            System.out.println ( x:"2 -> Open Account");
+            System.out.println ( x:"3 -> Close Account");
+            System.out.println ( x:"4 -> Display Customer Details");
+            System.out.println ( x:"5 -> Update Customer Details");
+            System.out.println ( x:"6 -> Delete Customer");
+            System.out.println ( x:"0 -> Exit")
+            System.out.print ( s:"Enter your choice: ");
 
-            System.out.println( "1 -> Register Customer");
-            System.out.println( "6 -> Delete Customer");
-            System.out.println( "0 -> Exit");
-            System.out.print( " Enter your choice :: " );            
             choice = sc.nextInt();
 
-            switch(choice){
-                case 0:
-                    System.exit(0);
-                case 1:
-                    System.out.println( " Enter Customer Id :: ");
-                    int custId = sc.nextInt();
-                    cObj = Customer.regCustomer(custId);
+            switch(int)
+            {
+                case 0 -> System.exit(status:0);
+                /*case 1 -> {
+                    System.out.println ( x:" Enter CustomerId : ");
+                    int custID = sc.nextInt();
+                    cObj = Customer.regCustomer (CustID);
                 }
+                case 2 -> 
+                case 3 -> 
+                case 4 -> 
+                case 5 -> 
+                case 6 -> */
+                
+
             }
+            
+            
+            
         }
     }
+
 }
