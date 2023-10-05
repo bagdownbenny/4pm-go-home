ebc42c3c50f3b90b9fa9d0c0041608977ffcd60e
bagdownbenny <benchan1004@gmail.com>
2023-10-05 11:34:21 +0800

    created case 2

--- capstone/BankingMain.java
+++ capstone/BankingMain.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class BankingMain{
 
     public static void main(String[] args){
-        Customer cObj;
         Scanner sc = new Scanner(System.in);
         int choice = 0;
+        Customer cObj; 
+        SavingsAcc sAcc;
         while (true){
             
             System.out.println ("1 -> Register Customer");
@@ -23,9 +24,10 @@ public class BankingMain{
             choice = sc.nextInt();
 
             switch(choice){
-                case 0 -> System.exit(0);
+                case 0 -> 
+                    System.exit(0);
                 case 1 -> {
-                    System.out.println("<<<< Register Customer >>>>");
+                    System.out.println("Welcome to Register Customer Page");
                     System.out.println( " Enter Customer Id :: ");
                     int custId = sc.nextInt();
                     System.out.println( " Enter Customer Name :: ");
@@ -38,18 +40,28 @@ public class BankingMain{
                     int phoneNo = sc.nextInt();
                     cObj = Customer.regCustomer(custId, custName, custUnique, custEmail, phoneNo);
                 }
-                case 2 -> //Open Account
+                case 2 -> {//Open Account
                     System.out.println("<<<< Open Account >>>>");
+                    System.out.println("What type of account would you like to open?");
+                    System.out.println("1 -> Savings Account");
+                    System.out.println("2 -> Current Account");
+                    int type = sc.nextInt();
+                    if (type == 1){
+                        sAcc = Customer.SavingsAcc(int accNo, String custUnique, String[] transactions, double balance, double interest);
+                    }
+                    if (type == 2){
+                        sAcc = Customer.CurrentAcc(int accNo, String custUnique, String[] transactions, double balance, double interest);
+                    }
+                    System.out.println("Congratulations! Your account has been successfully created");
+                    break;}
                 case 3 -> //Close Account
-                    System.out.println("<<<< Close Account >>>>");
+                System.out.println("<<<< Close Account >>>>");
                 case 4 -> //Display Customer Details
-                    System.out.println("<<<< Display Customer Details >>>>");
-                    System.out.println("All information");
-
+                System.out.println("<<<<Display Customer Details >>>>");
                 case 5 -> //Update Customer Details
-                    System.out.println("<<<< Update Customer Details >>>>");
+                System.out.println("<<<< Update Customer Details >>>>");
                 case 6 -> //Delete Customer 
-                    System.out.println(" <<<< Delete Customer & Details >>>>");
+                System.out.println(" <<<< Delete Customer & Details >>>>");
 
             }
             
