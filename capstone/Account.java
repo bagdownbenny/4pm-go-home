1677a0f7a95f167052a4ed27559697588fe1f965
GChristian <129513746+Jyuubey@users.noreply.github.com>
2023-10-05 11:24:10 +0800

    Update Account.java

--- capstone/Account.java
+++ capstone/Account.java
@@ -4,15 +4,42 @@ public class Account {
     int accNo;
     String custUnique;
     String [] transactions;
-    double balance;
+    double balance = 0;
+
+    public Account (int accNo, String custUnique, String[] transactions, double balance) {
+        this.accNo = accNo;
+        this.custUnique = custUnique;
+        this.transactions = transactions;
+        this.balance = balance;
+    }
+
+    
 }
 
 
 class SavingsAcc extends Account {
+    double interest = 0;
+
+    public SavingsAcc(int accNo, String custUnique, String[] transactions, double balance, double interest) {
+        super(accNo, custUnique, transactions, balance);
+        this.interest = interest;
+    }
+
+    
+    
 
 }
 
 
 class CheckingAcc extends Account{
+    double oveLimit = 0;
+    double overFees = 0;
+
+    public CheckingAcc(int accNo, String custUnique, String[] transactions, double balance, double oveLimit,
+            double overFees) {
+        super(accNo, custUnique, transactions, balance);
+        this.oveLimit = oveLimit;
+        this.overFees = overFees;
+    }
 
 }
