b2663a3505a5d47119d0d6bed79816772fa3d510
GChristian <129513746+Jyuubey@users.noreply.github.com>
2023-10-05 11:04:20 +0800

    Update Account.java

--- capstone/Account.java
+++ capstone/Account.java
@@ -1,5 +1,18 @@
 package capstone;
 
 public class Account {
-    
+    int accNo;
+    String custUnique;
+    String [] transactions;
+    double balance;
+}
+
+
+class SavingsAcc extends Account {
+
+}
+
+
+class CheckingAcc extends Account{
+
 }
