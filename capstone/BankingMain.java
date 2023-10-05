db8e0800a1ef7f1cafc57df7df855def56c56fb8
GChristian <129513746+Jyuubey@users.noreply.github.com>
2023-10-05 10:45:27 +0800

    Fill out case 1

--- capstone/BankingMain.java
+++ capstone/BankingMain.java
@@ -14,14 +14,21 @@ public class BankingMain {
             System.out.println( "0 -> Exit");
             System.out.print( " Enter your choice :: " );            
             choice = sc.nextInt();
-            
+
             switch(choice){
-                case 0:
-                    System.exit(0);
-                case 1:
+                case 0 -> System.exit(0);
+                case 1 -> {
                     System.out.println( " Enter Customer Id :: ");
                     int custId = sc.nextInt();
-                    cObj = Customer.regCustomer(custId);
+                    System.out.println( " Enter Customer Name :: ");
+                    String custName = sc.next();
+                    System.out.println( " Enter Customer Unique Identifier :: ");
+                    String custUnique = sc.next();
+                    System.out.println( " Enter Customer Email :: ");
+                    String custEmail = sc.next();
+                    System.out.println( " Enter Customer Phone Number :: ");
+                    int phoneNo = sc.nextInt();
+                    cObj = Customer.regCustomer(custId, custName, custUnique, custEmail, phoneNo);
                 }
             }
         }
