5006ea6f720b820574e78533a8ee5111ba95aae7
GChristian <129513746+Jyuubey@users.noreply.github.com>
2023-10-05 11:10:57 +0800

    Update BankingMain.java

--- capstone/BankingMain.java
+++ capstone/BankingMain.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class BankingMain{
 
-    public static void main[String[] args]{
+    public static void main(String[] args){
         Customer cObj;
         Scanner sc = new Scanner(System.in);
         int choice = 0;
